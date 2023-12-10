f40a1e1995a03fec8ff8e81a286a52b0b0049f5e
duong <ngducduong305@gmail.com>
2023-12-10 21:56:07 +0700

    Initialize component files

--- Project1/game.h
+++ Project1/game.h
@@ -1,5 +1,31 @@
 #pragma once
-class game
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
+
+enum GameState {PLAY, EXIT};
+
+class Game
 {
+public:
+	Game();
+	~Game();
+	void run();
+
+private:
+	void init(const char* title, int _x, int _y, int w, int h, Uint32 flags);
+
+	void gameLoop();
+
+	void handleEvents();
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	int screenWidth;
+	int screenHeight;
+
+	GameState gamestate;
 };
 
